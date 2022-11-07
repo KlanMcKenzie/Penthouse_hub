732de3a216b336d063a1aeb3d5ebf98eb7291a57
KlanMcKenzie <madollarprince254@gmail.com>
2022-11-07 12:35:42 +0300

    Added the screenshots directory

--- mypent/mypent/settings.py
+++ mypent/mypent/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5@lv3q1neg(l)1r+#9nh#se0t-^**=!&(_7t2ch=adxg%6#%4u'
+SECRET_KEY = xyz
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
